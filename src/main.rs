5e74cc63f4818715a3f1a904405644da7b28895a
Hakan Sonmez <hakan.sn.hs@gmail.com>
2024-01-14 20:26:40 +0300

    Implement memory module for page table abstractions

--- src/main.rs
+++ src/main.rs
@@ -6,9 +6,13 @@
 
 use core::panic::PanicInfo;
 
-use thanatos::println;
+use bootloader::{BootInfo, entry_point};
+use x86_64::structures::paging::Translate;
+use x86_64::VirtAddr;
 
-// TODO: Add a README.md file containing how to build and run the kernel
+use thanatos::{memory, println};
+
+// TODO: Add a README.md (build.rs would be even better) file containing how to build and run the kernel
 
 #[cfg(not(test))]
 #[panic_handler]
@@ -23,11 +27,32 @@ fn panic(info: &PanicInfo) -> ! {
     thanatos::test_panic_handler(info)
 }
 
-#[no_mangle]
-pub extern "C" fn _start() {
+entry_point!(kernel_main);
+
+pub fn kernel_main(boot_info: &'static BootInfo) -> ! {
     println!("Hello World{}", "!");
 
     thanatos::init();
+
+    let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
+    let mapper = unsafe { memory::init(phys_mem_offset) };
+
+    let addresses = [
+        // the identity-mapped vga buffer page
+        0xb8000,
+        // some code page
+        0x201008,
+        // some stack page
+        0x0100_0020_1a10,
+        // virtual address mapped to physical address 0
+        boot_info.physical_memory_offset,
+    ];
+
+    for &address in &addresses {
+        let virt = VirtAddr::new(address);
+        let phys = mapper.translate_addr(virt);
+        println!("{:?} -> {:?}", virt, phys);
+    }
 
     #[cfg(test)]
     test_main();
